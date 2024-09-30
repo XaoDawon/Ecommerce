4be35109cabb90e7ae077d51aa4a350fae077516
João Pedro <jpr.a.c119@gmail.com>
2024-09-29 22:01:50 -0300

    teste

--- rodape.php
+++ rodape.php
@@ -1,9 +1,8 @@
 <footer>
             <p id="a">Todos os direitos contritais reservados</p>
+            <p>o dudu gosta de dar o cu</p>
         </footer>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> 
-        <script src='./js/index.js'></script>
-        
+        <script src='./js/index.js'></script>  
     </body>
-
 </html>
