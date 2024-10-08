2ea36529d499e6aad0951fc19501a2b195309769
João Pedro <jpr.a.c119@gmail.com>
2024-10-07 21:28:19 -0300

    Tá quase

--- ExcluirUsuarios.php
+++ ExcluirUsuarios.php
@@ -4,17 +4,34 @@
   $id_usuario = $_GET['id'];
   $id_principal=$_GET['idprincipal'];
 
-  $varSQL = "DELETE FROM usuario WHERE id_usuario = :id_usuario";
-  $delete = $conn->prepare($varSQL);
-  $delete->bindParam(':id_usuario', $id_usuario);
+  $nome = "";
+  $tel = "";
+  $senha = "";
+  $excluido = true;
 
-  if($delete->execute()>0){
+  $varSQL = "UPDATE usuario SET nome=:nome, telefone=:telefone, senha=:senha, excluido=:excluido
+              WHERE id_usuario = :id_usuario";
+  $update = $conn->prepare($varSQL);
+  $update->bindParam(':id_usuario', $id_usuario);
+  $update->bindParam(':nome', $nome);
+  $update->bindParam(':telefone', $tel);
+  $update->bindParam(':senha', $senha);
+  $update->bindParam(':excluido', $excluido);
+
+  if($update->execute()>0){
       echo"Excluído";
   }
+  
   if($id_principal == NULL){
     echo "<br><a href='logout.php'>Voltar para o inicio</a>";
+    session_start();
+    session_destroy();
+    header('Location: index.php');
   }
   else{
     echo "<br><a href='usuarios.php?id=".$id_principal."'>Voltar </a>"; 
+    session_start();
+    session_destroy();
+    header('Location: index.php');
   }
 ?>
--- rodape.php
+++ rodape.php
@@ -1,23 +1,21 @@
 <footer>
-            <div class="a">
+            <div class="a"><center>
                 DESENVOLVEDORES<br><br>
-                Eduardo Martinez Pereira, n°8<br><br>
-                João Pedro Rodrigues Arruda Campos, n°15<br><br>
-                Pedro Henrique Almeida Trivelato, n°26<br><br>
-                Rafael dos Santos Ocon, n°29<br><br>
-                Rafaela Pinal de Mello, n°30<br><br>
-                Vinicius De Araujo Rossi, n°34
+                Eduardo Martinez Pereira, n°8<br>
+                João Pedro Rodrigues Arruda Campos, n°15<br>
+                Pedro Henrique Almeida Trivelato, n°26<br>
+                Rafael dos Santos Ocon, n°29<br>
+                Rafaela Pinal de Mello, n°30<br>
+                Vinicius De Araujo Rossi, n°34<br></center>
             </div>
-            <div class="a">
-                Instagram: @Festum__
+            <div class="a"><center>
+                Instagram: @Festum__</center>
             </div>
-            <div class="a">
-                Copyright © Festum 2024. Todos os direitos reservados.
-            </div>
-        </footer>
+            <div class="a"><center>
+                Copyright © Festum 2024. Todos os direitos reservados.</center>
+            </div> 
+        </footer> 
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> 
         <script src='./js/index.js'></script>
-        
     </body>
-
 </html>
